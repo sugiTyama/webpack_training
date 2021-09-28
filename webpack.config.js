877850499d27d30b4227151a299b65a685cf120c
t.sugiyama <90828369+sugiTyama@users.noreply.github.com>
2021-09-28 14:09:52 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
       },
       {
         test: /\.(png|jpg)/,
+        // type: 'asset/resource',
+        // generator: {
+        //     filename: 'images/[name][ext]'
+        // },
         use: [
           {
             loader: "file-loader",
@@ -34,6 +38,20 @@ module.exports = {
           },
         ],
       },
+      {
+          test: /\.pug/,
+          use: [
+              {
+                  loader: 'html-loader',
+              },
+              {
+                  loader: 'pug-html-loader',
+                  options: {
+                    pretty: true,
+                  }
+              }
+          ]
+      }
     ],
   },
   plugins: [
@@ -41,7 +59,12 @@ module.exports = {
       filename: "./stylesheets/main.css",
     }),
     new HtmlWebpackPlugin({
-      template: "./src/templates/index.html",
+      template: "./src/templates/index.pug",
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/access.pug",
+      filename: 'access.html',
     }),
     new CleanWebpackPlugin(),
   ],
